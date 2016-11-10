33c05eec91e35e9960e5291c7a7b3b1c90f41b25
Thaledric <thaledric@gmail.com>
2016-11-10 01:05:20 +0100

    Recherche

--- src/js/components/header.jsx
+++ src/js/components/header.jsx
@@ -9,11 +9,9 @@ class TfHeader extends React.Component {
     render(){
 
         return <header className="tf-site-header"><nav>
-            <a id="tf-main-menu-toggle"><Icon fixedWidth name="bars" /> Menu</a>
+            <a id="tf-main-menu-toggle"><Icon fixedWidth name="bars"/> Menu</a>
             <a id="tf-home-link" className="tf-site-title">TRIP FIGHTER</a>
-            <form className="tf-search-box">
-                <input type="search" name="search" className="tf-search-box-input"/>
-            </form>
+            <a id="tf-search-link" title="Recherchez du Japon"><Icon fixedWidth name="search"/>Rechercher</a>
             <a id="tf-login-box">Connexion</a>
         </nav></header>;
     }
