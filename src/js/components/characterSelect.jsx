a713aaad138886c6cfc7f43ab2d47719ad460100
Thaledric <thaledric@gmail.com>
2016-11-11 13:13:02 +0100

    Separators plus jolis

--- src/js/components/characterSelect.jsx
+++ src/js/components/characterSelect.jsx
@@ -7,19 +7,27 @@ class TfCharacterSelect extends React.Component {
     render(){
 
         return <section className="tf-character-select">
-            <div className="tf-character-left">
-                <a href="#" className="tf-character-select-link">
-                    <div className="tf-character-inner">
-                        <h3>Moderne</h3>
-                    </div>
-                </a>
-            </div>
-            <div className="tf-character-right">
-                <a href="#"  className="tf-character-select-link">
-                    <div className="tf-character-inner">
-                        <h3>Traditionnel</h3>
-                    </div>
-                </a>
+            <h2>
+                Select your challenger
+            </h2>
+            <p className="center">
+                Choisissez un univers et commencez votre voyage culturel
+            </p>
+            <div className="tf-character-select-selector">
+                <div className="tf-character-left">
+                    <a href="#" className="tf-character-select-link">
+                        <div className="tf-character-inner">
+                            <h3>Moderne</h3>
+                        </div>
+                    </a>
+                </div>
+                <div className="tf-character-right">
+                    <a href="#"  className="tf-character-select-link">
+                        <div className="tf-character-inner">
+                            <h3>Traditionnel</h3>
+                        </div>
+                    </a>
+                </div>
             </div>
         </section>;
     }
--- src/js/components/separator.jsx
+++ src/js/components/separator.jsx
@@ -7,6 +7,8 @@ class TfSeparator extends React.Component {
     render(){
         return <div className="tf-separator">
             <div className="tf-separator-icon"></div>
+            <div className="tf-separator-icon"></div>
+            <div className="tf-separator-icon"></div>
         </div>;
     }
 
