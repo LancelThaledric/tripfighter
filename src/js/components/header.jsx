19fa74c19268dc42d3fcbef04f37d87f11194c77
Thaledric <thaledric@gmail.com>
2016-11-21 17:47:49 +0100

    header propre

--- src/js/components/header.jsx
+++ src/js/components/header.jsx
@@ -4,15 +4,24 @@ import React from 'react';
 
 import {Icon} from 'react-fa';
 
+import TfAButton from './abutton.jsx';
+
 class TfHeader extends React.Component {
 
     render(){
 
         return <header className="tf-site-header"><nav>
-            <a id="tf-main-menu-toggle"><Icon fixedWidth name="bars"/> Menu</a>
-            <a id="tf-home-link" className="tf-site-title">TRIP FIGHTER</a>
-            <a id="tf-search-link" title="Recherchez du Japon"><Icon fixedWidth name="search"/>Rechercher</a>
-            <a id="tf-login-box">Connexion</a>
+            <div>
+                <TfAButton href="#" id="tf-main-menu-toggle"><Icon fixedWidth name="bars"/> Menu</TfAButton>
+            </div>
+            <div>
+                <a href="#" id="tf-home-link" className="tf-site-title">TRIP FIGHTER</a>
+            </div>
+            <div>
+                <TfAButton href="#" id="tf-search-link" title="Recherchez du Japon"><Icon fixedWidth name="search"/></TfAButton>
+                <TfAButton href="#" id="tf-login-box" title="Connexion / Inscription"><Icon fixedWidth name="user"/></TfAButton>
+            </div>
+            
         </nav></header>;
     }
 
