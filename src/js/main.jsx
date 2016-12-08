a2ebdc1155afc38074b07db5f9ef043460c7dd2c
Thaledric <thaledric@gmail.com>
2016-12-08 15:38:04 +0100

    liens du menu plus sympa

--- src/js/main.jsx
+++ src/js/main.jsx
@@ -1,17 +1,17 @@
 import 'babel-polyfill';
-
+// React
 import ReactDOM from 'react-dom';
 import React from 'react';
 import { Router, Route, IndexRoute, Link, browserHistory } from 'react-router';
-
-import index from 'file?name=[name].[ext]!../index.html';
-import style from '../style/style.scss';
-
-import '../.htaccess';
-
+// Components
 import TfHome from './components/pages/tfhome/tfhome.jsx';
 import TfUnivers from './components/pages/tfunivers/tfunivers.jsx';
 import Tf404 from './components/pages/tf404/tf404.jsx';
+// Style
+import style from '../style/style.scss';
+// Resources
+import index from 'file?name=[name].[ext]!../index.html';
+import '../.htaccess';
 
 ReactDOM.render(
     <Router history={browserHistory}>
