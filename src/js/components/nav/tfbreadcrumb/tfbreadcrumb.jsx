286bb08dccf28c3d7c3b645d156b9faf4794fae3
Thaledric <thaledric@gmail.com>
2016-12-15 11:59:39 +0100

    Correction liens mort dans fil d'ariane

--- src/js/components/nav/tfbreadcrumb/tfbreadcrumb.jsx
+++ src/js/components/nav/tfbreadcrumb/tfbreadcrumb.jsx
@@ -23,9 +23,9 @@ class TfBreadcrumb extends React.Component {
         if(this.props.univers)
             links.push(<Icon name="angle-right" key={'icon-univers-'+this.props.univers.slug}/>, <Link to={Url.computeUrl(this.props.univers.slug)} key={'univers-'+this.props.univers.slug}>{this.props.univers.name}</Link>);
         if(this.props.theme)
-            links.push(<Icon name="angle-right" key={'icon-theme-'+this.props.theme.slug}/>, <Link to={Url.computeUrl(this.props.theme.slug)} key={'theme-'+this.props.theme.slug}>{this.props.theme.name}</Link>);
+            links.push(<Icon name="angle-right" key={'icon-theme-'+this.props.theme.slug}/>, <Link to={Url.computeUrl('', this.props.theme.slug)} key={'theme-'+this.props.theme.slug}>{this.props.theme.name}</Link>);
         if(this.props.articleTitle && this.props.articleSlug)
-            links.push(<Icon name="angle-right" key={'icon-article-'+this.props.articleSlug}/>, <Link to={Url.computeUrl(this.props.articleSlug)} key={'article-'+this.props.articleSlug}>{this.props.articleTitle}</Link>);
+            links.push(<Icon name="angle-right" key={'icon-article-'+this.props.articleSlug}/>, <Link to={Url.computeUrl('', '', this.props.articleSlug)} key={'article-'+this.props.articleSlug}>{this.props.articleTitle}</Link>);
         
         // Calcul du switcher
         // 1 : univers
